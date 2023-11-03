117c2f139911ec18887e05796c8a265a5ef5abed
juan <jgarmar2710@g.educaand.es>
2023-11-03 13:56:10 +0100

    cambio

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int tamanyo= sc.nextInt();
-        int medio=tamanyo/2;
-        if (tamanyo%2!=0){
-            for (int i=0;i<tamanyo;i++){
-                if (tamanyo>1&&i!=medio){
-                    for (int j=0;j<tamanyo-medio-1;j++){
+        Scanner sc = new Scanner(System.in);
+        int tamanyo = sc.nextInt();
+        int medio = tamanyo / 2;
+        if (tamanyo % 2 != 0) {
+            for (int i = 0; i < tamanyo; i++) {
+                if (tamanyo > 1 && i != medio) {
+                    for (int j = 0; j < tamanyo - medio - 1; j++) {
                         System.out.print(" ");
                     }
                 }
-                if (i!=medio) System.out.print("X");
-                if (i==medio){
-                    for (int j=0;j<tamanyo;j++){
+                if (i != medio) System.out.print("X");
+                if (i == medio) {
+                    for (int j = 0; j < tamanyo; j++) {
                         System.out.print("X");
                     }
                 }
-                if (tamanyo>1&&i!=medio){
-                    for (int j=0;j<tamanyo-medio-1;j++){
+                if (tamanyo > 1 && i != medio) {
+                    for (int j = 0; j < tamanyo - medio - 1; j++) {
                         System.out.print(" ");
                     }
                 }
                 System.out.println();
             }
-        }else System.out.println("Numero no valido");
+        } else System.out.println("Numero no valido");
     }
 }
