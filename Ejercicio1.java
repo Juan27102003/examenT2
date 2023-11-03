2a48cbb4937efd30899aeb80df97c1a8082973ce
juan <jgarmar2710@g.educaand.es>
2023-11-03 13:16:34 +0100

    cambio

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int num= sc.nextInt();
-        if (num%2==0){
-            for (int i=10;i<num;i++){
-                if (i%5==0) System.out.println(i);
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            for (int i = 10; i < num; i++) {
+                if (i % 5 == 0) System.out.println(i);
             }
-        }else {
-            for (int i=10;i<num;i++){
-                if (i%3==0) System.out.println(i);
+        } else {
+            for (int i = 10; i < num; i++) {
+                if (i % 3 == 0) System.out.println(i);
             }
         }
     }
--- Ejercicio2.java
+++ Ejercicio2.java
@@ -4,20 +4,20 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nota, media = 0;
-        int contSus = 0, cont = 0,contApro = 0;
+        int contSus = 0, cont = 0, contApro = 0;
         do {
             nota = sc.nextInt();
-            if (nota>0){
-            if (nota >= 5) contApro++;
-            if (nota < 5) contSus++;
-            cont++;
-            media += nota;
+            if (nota > 0) {
+                if (nota >= 5) contApro++;
+                if (nota < 5) contSus++;
+                cont++;
+                media += nota;
             }
         } while (nota != -1);
         media = media / cont;
-        if (cont==0){
+        if (cont == 0) {
             System.out.println("No se han introducido calificaciones");
-        }else{
+        } else {
             System.out.println("La media es " + media);
             System.out.println("Aprobados: " + contApro + " - Suspensos: " + contSus);
         }
